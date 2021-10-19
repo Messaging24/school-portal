194b3195c3e962050a8f67007607c63cd2422a89
mihmosi <mihmosi@gmail.com>
2021-10-19 11:47:11 +0300

    в SecurityConfig в configure(HttpSecurity http) включите CORS и отключите CSR, установите управление сеансом

--- school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
@@ -11,11 +11,66 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // TODO configure authentication manager
+
     }
+
+// TODO: Установите управление сеансом.
+// TODO: Установить обработчик исключений неавторизованных запросов.
+// TODO: Установите разрешения для конечных точек.
+// TODO: Добавить фильтр токенов JWT.
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // TODO configure web security
+        http
+                // TODO: 18.10.2021 Включите CORS и отключите CSRF
+                .csrf().disable()
+                .cors()
+                .and()
+
+                // TODO: Установите управление сеансом.
+                .authorizeRequests()
+                .antMatchers("/user/**").access("hasAnyRole('HEADTECHER','TEACHER','PARENT','STUDENT','DIRECTOR')")
+
+                .antMatchers("/**").access("hasAnyRole('DIRECTOR')")
+
+                .and()
+                // TODO: Установить обработчик исключений неавторизованных запросов.
+                // TODO to build classes SimpleAccessDeniedHandler and SimpleAuthenticationEntryPoint
+//                .exceptionHandling().accessDeniedHandler(new SimpleAccessDeniedHandler()).authenticationEntryPoint(new SimpleAuthenticationEntryPoint())
+
+                .formLogin()  // Spring сам подставит свою логин форму
+//                .loginProcessingUrl("/j_spring_security_check")
+                .loginPage("/login")
+                .defaultSuccessUrl("/user", true)
+
+                .failureUrl("/login?error=true")
+                .usernameParameter("email")
+//                .passwordParameter("password")
+                // TODO
+                // .successHandler()// подключаем наш SuccessHandler для перенеправления по ролям
+                .permitAll()
+// TODO  Добавить фильтр токенов JWT.
+//            .and()
+//            .oauth2ResourceServer().jwt() далее конфигурация
+
+                .and()
+                .logout()
+                .logoutUrl("/logout") //URL-адрес, запускающий выход из системы (по умолчанию "/ logout").
+                .logoutSuccessUrl("/");//URL-адрес для перенаправления после выхода из системы
     }
+
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        final CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(ImmutableList.of("*"));
+//        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+//        final source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }UrlBasedCorsConfigurationSource
 
 }
