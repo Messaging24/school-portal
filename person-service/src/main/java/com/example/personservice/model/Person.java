a49bb3ec4948e8576b2384462d70bd7b1cc59da1
PavelSav <pavelsavosinw@gmail.com>
2021-10-19 11:58:27 +0300

    SP-SJ-3061 - Добавить пакет models для блока person-service + Абстрактный класс Person

---
+++ person-service/src/main/java/com/example/personservice/model/Person.java
@@ -0,0 +1,50 @@
+package com.example.personservice.model;
+
+public abstract class Person {
+
+    private String firstName;
+    private String surName;
+    private String patronymic;
+    private char gender;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
