59df3e468e248219b0ba3b8dce6c301e0e5f3431
Sergei <ivanov.sappleid@gmail.com>
2021-10-19 13:49:50 +0500

    SP-SJ-3071 - Настроить SecurityConfig: configure(AuthenticationManagerBuilder auth)

--- school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
@@ -1,16 +1,44 @@
 package com.jm.project.schooljournal.config;
 
+import com.jm.project.schooljournal.model.User;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // TODO configure authentication manager
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username)
+                    throws UsernameNotFoundException {
+                //Ищем юзера в бд
+                User user = userRepository.findUserByUsername(username);
+                if (user != null) {
+                    //Если нашли, возвращаем UserDetails
+                    return new org.springframework.security.core.userdetails.User(
+                            user.getUsername(),
+                            user.getPassword(),
+                            user.getAuthorities()
+                    );
+                }
+                //Если не нашли, бросаем исключение
+                throw new UsernameNotFoundException("User '" + username + "' not exists");
+            }
+        });
     }
 
     @Override
