2675a1ea304338a07ffcf0918944e1c0b1450d9c
nlc1 <83986068+nlc1@users.noreply.github.com>
2021-10-20 22:22:16 +0500

    SP-SJ-3051 Create class user

--- school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
@@ -1,4 +1,48 @@
 package com.jm.project.schooljournal.model;
 
-public class User {
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(email = "uuid", unique = true)
+    private int uuid;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "soname")
+    private String soname;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(int uuid, Set<Role> role, String name, String soname, String patronymic, int age, String password) {
+        this.uuid = uuid;
+        this.role = role;
+        this.name = name;
+        this.soname = soname;
+        this.patronymic = patronymic;
+        this.age = age;
+        this.password = password;
+    }
 }
+
