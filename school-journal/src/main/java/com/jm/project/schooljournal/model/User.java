c36909bdd2486b61dc2fb3499d9fc2f45587d56e
nlc1 <83986068+nlc1@users.noreply.github.com>
2021-10-20 12:54:29 +0500

    SP-JS-3051 Create User.java

--- school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
@@ -1,4 +1,44 @@
 package com.jm.project.schooljournal.model;
 
-public class User {
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(email = "uuid", unique = true)
+    private int uuid;
+
+    @Column(role = "role")
+    private int role;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "soname")
+    private String soname;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(int uuid, String role, String name, String soname, String patronymic, int age, String password) {
+        this.uuid = uuid;
+        this.role = role;
+        this.name = name;
+        this.soname = soname;
+        this.patronymic = patronymic;
+        this.age = age;
+        this.password = password;
+    }
 }
