c16c50f409e2e24b7e5ced35011a6b1da8eb46f8
nlc1 <83986068+nlc1@users.noreply.github.com>
2021-10-22 01:00:18 +0500

    SP-JS-3051 Add implementation methods

--- school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
@@ -1,11 +1,13 @@
 package com.jm.project.schooljournal.model;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import javax.management.relation.Role;
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
+
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User implements UserDetails {
 
     @Id
@@ -41,4 +43,27 @@ public class User implements UserDetails {
         this.password = password;
         this.roles = roles;
     }
+    public User() {      
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return null; }
+
+    @Override
+    public String getPassword() { return null; }
+
+    @Override
+    public String getUsername() { return null; }
+
+    @Override
+    public boolean isAccountNonExpired() { return false; }
+
+    @Override
+    public boolean isAccountNonLocked() { return false; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return false; }
+
+    @Override
+    public boolean isEnabled() { return false; }
 }
