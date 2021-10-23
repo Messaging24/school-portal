138448d6af7f0fb52d99365f468487b2287a8e3e
nlc1 <83986068+nlc1@users.noreply.github.com>
2021-10-23 23:47:07 +0500

    SP-SJ-3053 Add username and pwd

--- school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/model/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Set;
+
 @Entity
 @Table(name = "user")
 public class User implements UserDetails {
@@ -30,29 +31,29 @@ public class User implements UserDetails {
         this.password = password;
         this.roles = roles;
     }
-    public User() {
-    }
+
+    public User() {}
 
     public String getUserName() { return userName; }
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() { return null; }
+    public Collection<? extends GrantedAuthority> getAuthorities() { return roles; }
 
     @Override
-    public String getPassword() { return null; }
+    public String getPassword() { return password; }
 
     @Override
-    public String getUsername() { return null; }
+    public String getUsername() { return userName; }
 
     @Override
-    public boolean isAccountNonExpired() { return false; }
+    public boolean isAccountNonExpired() { return true; }
 
     @Override
-    public boolean isAccountNonLocked() { return false; }
+    public boolean isAccountNonLocked() { return true; }
 
     @Override
-    public boolean isCredentialsNonExpired() { return false; }
+    public boolean isCredentialsNonExpired() { return true; }
 
     @Override
-    public boolean isEnabled() { return false; }
+    public boolean isEnabled() { return true; }
 }
