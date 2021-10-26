2c4aa77faf199076cca2e5c9fc091daff2ad24fc
mihmosi <mihmosi@gmail.com>
2021-10-26 15:03:45 +0300

    есть доступ на /

--- school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
+++ school-journal/src/main/java/com/jm/project/schooljournal/config/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.jm.project.schooljournal.config;
 
+import com.jm.project.schooljournal.config.jwt.AuthEntryPointJwt;
+import com.jm.project.schooljournal.config.jwt.AuthTokenFilter;
 import com.jm.project.schooljournal.model.User;
 import com.jm.project.schooljournal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+// TODO =====================================
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+// TODO =====================================
 
     private UserRepository userRepository;
 
@@ -39,8 +53,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // TODO configure web security
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().permitAll();
 
-
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
     @Bean
