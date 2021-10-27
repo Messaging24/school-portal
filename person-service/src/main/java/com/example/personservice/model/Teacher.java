857f5e97bbbad96cca3cdef5fd06c5e2b9cbcda4
nlc1 <83986068+nlc1@users.noreply.github.com>
2021-10-27 19:37:27 +0500

    Add class Teacher

---
+++ person-service/src/main/java/com/example/personservice/model/Teacher.java
@@ -0,0 +1,34 @@
+package com.example.personservice.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher extends Person {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @OneToMany(mappedBy = "id",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<Classes> classesList;
+
+    public Teacher() {
+
+    }
+
+    public Teacher(String firstName, String lastName, String secondName, char gender, int age, Long id, List<Classes> classesList) {
+        super(firstName, lastName, secondName, gender, age);
+        this.id = id;
+        this.classesList = classesList;
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+}
