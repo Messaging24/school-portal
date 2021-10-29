f0b6ae61b27d3b518e82bde9bd645944e1ed69f5
limenkey <renlimenki@gmail.com>
2021-10-29 11:57:01 +0300

    SP-SJ-3015 - Добавить страницу /school-portal/home

---
+++ static-module/src/common/city-form/index.tsx
@@ -0,0 +1,55 @@
+import { Card, Select } from "antd"
+import { connect } from "react-redux"
+import { AnyAction, bindActionCreators, Dispatch } from "redux"
+import { setCity } from "../../components/app/appSlice"
+import { stateFromStore } from "../../typescript/types"
+
+
+const cities = [
+    "Москва",
+    "Санкт-Петербург",
+    "Ростов-на-Дону",
+    "Нижний Новгород",
+    "Казань",
+    "Волгоград",
+    "Ставрополь",
+]
+
+const CityForm = ({setCity} : {setCity?:any}) => {
+    const { Option } = Select
+
+    const handleSelect = (value:any) => setCity(value)
+
+    const options = cities.map(( item ) => 
+        <Option 
+            className="option-city" 
+            key={item} 
+            value={item}
+            >
+        {item}
+        </Option>)
+
+    return (
+        <div className="city-form-wrapper">
+            <Card>
+                <Select
+                    dropdownClassName="select-city"
+                    placeholder="Выберите ваш город" 
+                    dropdownMatchSelectWidth
+                    onChange={handleSelect}>
+                    {options}
+                </Select>
+            </Card>
+        </div>
+    )
+}
+
+const mapState = (state:stateFromStore) => ({})
+const mapDispatch = (dispatch: Dispatch<AnyAction>) => {
+    const bound = bindActionCreators({setCity}, dispatch)
+    return {
+        setCity: bound.setCity
+    }
+}
+
+export default connect(mapState, mapDispatch)(CityForm)
--- static-module/src/components/app/appSlice.ts
+++ static-module/src/components/app/appSlice.ts
@@ -1,23 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { appState } from '../../typescript/types'
 
-
 const initialState: appState = {
   isLoggedIn: false,
-  currentPage: 'sign-in',
   currentUser: {
     position: 'director',
     login: 'test',
     name: 'any'
   },
   serverErrors: false,
+  city: false,
 }
 
 const appSlice = createSlice({
   name: 'app',
   initialState,
-  reducers: {},
+  reducers: {
+    setCity: (state, action) => {
+      state.city = action.payload
+    },
+  },
+  extraReducers: {
+  },
 })
 
-
+export const { setCity } = appSlice.actions
 export default appSlice.reducer
