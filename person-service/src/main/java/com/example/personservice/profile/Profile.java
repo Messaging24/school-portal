acab52cb160c1bfed7166a32048368b50c92bf53
Daniil1399 <danil1399danil@yandex.ru>
2021-11-01 23:47:41 +0300

    SP-SJ-3029 добавлены перечисления профилей

---
+++ person-service/src/main/java/com/example/personservice/profile/Profile.java
@@ -0,0 +1,12 @@
+package com.example.personservice.profile;
+
+public enum Profile {
+
+    physical_mathematical,
+    chemical_biological,
+    information_technology,
+    socio_economic,
+    humanitarian,
+    linguistic,
+    artistic_aesthetic
+}
