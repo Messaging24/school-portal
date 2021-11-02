f8b5e072b2532992adc3cb8a01b93bc573b5802a
mihmosi <mihmosi@gmail.com>
2021-11-02 12:04:01 +0300

    Добавиль класс enum Предметы в personservice/model/schoolattributes/Items

---
+++ person-service/src/main/java/com/example/personservice/model/schoolattributes/Items.java
@@ -0,0 +1,40 @@
+package com.example.personservice.model.schoolattributes;
+
+public enum Items {
+    CALLIGRAPHY,                          // Чистописание
+    READING,                              // Чтение
+    LABOR,                                // Труд
+    NATURAL_SCIENCE,                      // Природоведение
+    MATHEMATICS,                          // Математика
+    MUSIC_SINGING,                        // Музыка (Пение)
+    FINE_ARTS_DRAWING,                    // Изобразительное искусство (Рисование)
+    RUSSIAN_LANGUAGE,                     // Русский язык
+    PHYSICAL_CULTURE,                     // Физкультура
+    MOTHER_TONGUE,                        // Родной язык
+    FUNDAMENTALS_OF_RELIGIOU_CULTURES_AND_SECULAR_ETHICS, // Основы религиозных культур и светской этики
+    FOREIGN_LANGUAGE,                     // Иностранный язык
+    CITIZENSHIP,                          // Граждановедение
+    LOCAL_HISTORY,                        // Краеведение
+    HISTORY,                              // История
+    LITERATURE,                           // Литература
+    FUNDAMENTALS_OF_LIFE_SAFty_OBZH,      // Основы безопасности жизнедеятельности (ОБЖ)
+    TECHNOLOGY,                           // Технология
+    GEOGRAPHY,                            // География
+    BIOLOGY,                              // Биология
+    INFORMATICS,                          // Информатика
+    SOCIAL_SCIENCE,                       // Обществознание
+    DRAWING,                              // Черчение
+    ALGEBRA,                              // Алгебра
+    GEOMETRY,                             // Геометрия
+    PHYSICS,                              // Физика
+    CHEMISTRY,                            // Химия
+    SCIENCE,                              // Естествознание
+    FUNDAMENTALS_OF_ECONOMIC,             // Основы экономики
+    JURISPRUDENCE,                        // Правоведение
+    PHILOSOPHY,                           // Философия
+    ECOLOGY,                              // Экология
+    ASTRONOMY,                            // Астрономия
+    BASIC_MILITARY_TRAINING_BMT,          // Начальная военная подготовка (НВП)
+    WORLD_ART_CULTUREMHC_OPTIONAL,        // Мировая художественная культура (МХК) факультативно
+    RHETORIC_OPTIONAl,                    // Риторика факультативно
+}
