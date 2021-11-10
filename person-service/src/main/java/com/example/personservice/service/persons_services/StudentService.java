7cc407dcce0036c55ede8a1caed359a68bd3632a
Nikita <SOTUSs999@gmail.com>
2021-11-10 18:57:48 +0300

    исправил сервис

--- person-service/src/main/java/com/example/personservice/service/persons_services/StudentService.java
+++ person-service/src/main/java/com/example/personservice/service/persons_services/StudentService.java
@@ -4,7 +4,7 @@ import com.example.personservice.model.persons.Student;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
+
 public interface StudentService {
     void addStudent(Student parent);
 
